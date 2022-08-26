5e2a125c36d6f3b1d0d3a2d0fe6cbff409cd99f3
Thuong. Tran Kim  - CMC Global DU1.18 <tkthuong@cmcglobal.vn>
2022-08-26 18:09:03 +0700

    commonJs module

---
+++ starter/script.js
@@ -0,0 +1,78 @@
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+// console.log('Importing module');
+// // console.log(shippingCost);
+
+// addToCart('Bread', 5);
+// console.log(price);
+// console.log(tq);
+
+// ///////////////////////////////////
+// // Cach 2:
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('Break', 10);
+// console.log(ShoppingCart.totalPrice);
+
+import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+add('rice', 5);
+
+console.log(price);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// // Not very clean
+// // lastPost.then(last => console.log(last));
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+///////////////////////////////////////
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
